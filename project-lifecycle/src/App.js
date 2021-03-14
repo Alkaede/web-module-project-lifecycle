4f29e7c435347494591de92ad03cb6a332c02bfa
Alkaede <andrewcast32@gmail.com>
2021-03-14 14:25:12 -0700

    CRA started, switched to sass, and added dependencies

---
+++ project-lifecycle/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './components/styles.scss'
+
+class App extends React.Component{
+  // state ={
+
+  // }
+
+  render(){
+    return(
+      <div className="App">
+        <p>Its time to start making lifecycles :D</p>
+        <p>and try out sass</p>
+      </div>
+    )
+  }
+}
+
+export default App;
